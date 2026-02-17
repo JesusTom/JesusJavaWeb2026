e59fd53c1180b40dbd8fb29f56f8e5c0777577b8
JesusTom <jesus.tomas.rubio@gmail.com>
2026-02-17 12:12:24 +0100

    Personalize welcome message with user name

--- src/main/java/ejemplos/servlet/curso/MyServlet2.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet2.java
@@ -14,6 +14,8 @@ public class MyServlet2 extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
+
+		String name = (String) request.getParameter("name");
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 
@@ -21,7 +23,7 @@ public class MyServlet2 extends HttpServlet {
 		out.println("<html>");
 		out.println("<head><title>Ejemplo HTML desde Servlet</title></head>");
 		out.println("<body>");
-		out.println("<h1>Bienvenido!!</h1>");
+		out.println("<h1>Bienvenido"+name+"!!</h1>");
 	}
 
 }
